821b6445ae32c8cacec83dd21dd2ba9bad4dbfbf
Jeremy Hall <jeremyhallpdx@gmail.com>
2017-10-29 15:12:35 -0700

    starting trackRound activity

--- app/src/main/java/com/jeremyhallpdx/averagejoeshottimer/MainActivity.java
+++ app/src/main/java/com/jeremyhallpdx/averagejoeshottimer/MainActivity.java
@@ -4,13 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button trackNewRound = findViewById(R.id.button_track_new_round);
+
+        trackNewRound.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                trackRound(view);
+            }
+        });
     }
 
     public void trackRound(View view) {
--- app/src/main/java/com/jeremyhallpdx/averagejoeshottimer/TrackRound.java
+++ app/src/main/java/com/jeremyhallpdx/averagejoeshottimer/TrackRound.java
@@ -1,13 +1,42 @@
 package com.jeremyhallpdx.averagejoeshottimer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class TrackRound extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        TextView displayTimer;
+        Button shooterReady;
+        ListView shotsRecorded;
+
+        long millisecondTime, startTime, timeBuff, updateTime = 0L;
+        int minutes, seconds, milliseconds;
+
+        ArrayAdapter<String> adapter;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track_round);
+
+        shooterReady = findViewById(R.id.button_shooter_ready);
+        displayTimer = findViewById(R.id.display_shot_timer);
+        shotsRecorded = findViewById(R.id.listview_shots_recorded);
+
+        shooterReady.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                // Make a "beep" to start the stopwatch and access the microphone to record shots.
+                // Also must display the "shots" in the listView
+            }
+        });
     }
 }
