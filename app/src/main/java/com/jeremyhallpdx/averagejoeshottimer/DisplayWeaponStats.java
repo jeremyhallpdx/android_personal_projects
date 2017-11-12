40ff794edb1df3db672e39082e48a7832f6b909f
Jeremy Hall <jeremyhallpdx@gmail.com>
2017-11-11 17:53:42 -0800

    added newRound buttons to DisplayWeaponStats activity

--- app/src/main/java/com/jeremyhallpdx/averagejoeshottimer/DisplayWeaponStats.java
+++ app/src/main/java/com/jeremyhallpdx/averagejoeshottimer/DisplayWeaponStats.java
@@ -1,15 +1,30 @@
 package com.jeremyhallpdx.averagejoeshottimer;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class DisplayWeaponStats extends MainActivity {
+
+    private static final String TAG = "DisplayWeaponStats";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_weapon_stats);
+
+        Button button = findViewById(R.id.button_track_new_round);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                trackRound(view);
+            }
+        });
     }
 
     @Override
@@ -17,7 +32,18 @@ public class DisplayWeaponStats extends MainActivity {
 
         getMenuInflater().inflate(R.menu.stats_menu, menu);
 
-        String checkWeapon = getIntent().getExtras().get(WEAPON_TYPE).toString();
+        // ask Fasching about this try/catch.  how best to implement this check?
+        String checkWeapon = "";
+
+        try {
+
+            checkWeapon = getIntent().getExtras().get(WEAPON_TYPE).toString();
+        }
+
+        catch (NullPointerException e) {
+
+            Log.d(TAG, "onCreateOptionsMenu: Error in intent bundle: " + e.getMessage());
+        }
 
         switch (checkWeapon) {
 
